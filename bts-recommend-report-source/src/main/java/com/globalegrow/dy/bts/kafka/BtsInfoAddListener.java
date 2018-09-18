e8fce14ef6fb6f21e903c6e8ff71c245f8dffd57
timewang <timewmoon@163.com>
2018-09-18 11:44:04 +0800

    APP 代码

--- bts-recommend-report-source/src/main/java/com/globalegrow/dy/bts/kafka/BtsInfoAddListener.java
+++ bts-recommend-report-source/src/main/java/com/globalegrow/dy/bts/kafka/BtsInfoAddListener.java
@@ -22,7 +22,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 
-//@Component
+@Component
 @ConfigurationProperties(prefix = "app.kafka")
 public class BtsInfoAddListener {
 
