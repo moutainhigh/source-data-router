9baedd80f7789b072f443a68b70d296e70e57fc0
timewang <timewmoon@163.com>
2018-10-09 18:45:21 +0800

    并发设置

--- http-kylin-decorate/src/main/java/com/globalegrow/dy/config/RestTemplateConfig.java
+++ http-kylin-decorate/src/main/java/com/globalegrow/dy/config/RestTemplateConfig.java
@@ -53,9 +53,9 @@ public class RestTemplateConfig {
                 .build();
         PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager(registry);
         //设置整个连接池最大连接数 根据自己的场景决定
-        connectionManager.setMaxTotal(1000);
+        connectionManager.setMaxTotal(500);
         //路由是对maxTotal的细分
-        connectionManager.setDefaultMaxPerRoute(30);
+        connectionManager.setDefaultMaxPerRoute(10);
         RequestConfig requestConfig = RequestConfig.custom()
                 .setSocketTimeout(20000) //服务器返回数据(response)的时间，超过该时间抛出read timeout
                 .setConnectTimeout(20000)//连接上服务器(握手成功)的时间，超出该时间抛出connect timeout
