74ecadfe80b36bde177df1b86086e0a354dd3e7a
timewang <timewmoon@163.com>
2018-10-17 15:28:41 +0800

    参数校验取数

--- http-kylin-decorate/src/main/java/com/globalegrow/dy/controller/UserActionController.java
+++ http-kylin-decorate/src/main/java/com/globalegrow/dy/controller/UserActionController.java
@@ -5,6 +5,7 @@ import com.globalegrow.dy.dto.UserActionResponseDto;
 import com.globalegrow.dy.service.RealTimeUserActionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ public class UserActionController {
     private RealTimeUserActionService realTimeUserActionEsServiceImpl;
 
     @RequestMapping(value = "userActionResponseDto",produces = "application/json;charset=UTF-8", method = RequestMethod.POST)
-    public UserActionResponseDto userAction(@Validated UserActionParameterDto parameterDto) throws IOException {
+    public UserActionResponseDto userAction(@Validated @RequestBody UserActionParameterDto parameterDto) throws IOException {
         UserActionResponseDto userActionResponseDto = new UserActionResponseDto();
         userActionResponseDto.setData(this.realTimeUserActionEsServiceImpl.userActionData(parameterDto));
         return userActionResponseDto;
