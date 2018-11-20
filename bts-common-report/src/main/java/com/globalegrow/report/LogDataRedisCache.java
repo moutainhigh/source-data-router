5dca056c63954bb6630a04864fb9ef825f1495cb
timewang <timewmoon@163.com>
2018-11-20 09:38:41 +0800

    用户 id cookie 关联关系保存

--- bts-common-report/src/main/java/com/globalegrow/report/LogDataRedisCache.java
+++ bts-common-report/src/main/java/com/globalegrow/report/LogDataRedisCache.java
@@ -1,5 +1,6 @@
 package com.globalegrow.report;
 
+import com.globalegrow.common.hbase.CommonHbaseMapper;
 import com.globalegrow.util.JacksonUtil;
 import com.globalegrow.util.MD5CipherUtil;
 import com.globalegrow.util.SpringRedisUtil;
@@ -12,7 +13,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -27,6 +27,9 @@ public class LogDataRedisCache implements LogDataCache {
     private JestClient jestClient;
 
     public static final String APP_REPORT_END_FLAG = "_APP";
+
+    @Autowired
+    private CommonHbaseMapper commonHbaseMapper;
 
     /**
      * 将埋点数据进行缓存主要为加购数据缓存,目前只支持加购信息缓存
