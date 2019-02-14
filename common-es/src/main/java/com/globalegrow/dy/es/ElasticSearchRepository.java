498bae3b06f1eaa90516bfd63281fe5468925d77
timewang <timewmoon@163.com>
2019-02-14 14:35:31 +0800

    通用 in 查询

--- common-es/src/main/java/com/globalegrow/dy/es/ElasticSearchRepository.java
+++ common-es/src/main/java/com/globalegrow/dy/es/ElasticSearchRepository.java
@@ -3,11 +3,16 @@ package com.globalegrow.dy.es;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 @Repository
@@ -40,6 +45,18 @@ public class ElasticSearchRepository {
                 .actionGet();
     }
 
+    public List<Map<String, Object>> idInSearch(String index, String type, List<String> ids) {
+        List<Map<String, Object>> mapList = new ArrayList<>();
+        Arrays.stream(this.client.prepareSearch(index).setTypes(type)
+                .setQuery(QueryBuilders.termsQuery("_id", ids)).get().getHits().getHits()).forEach(searchHitFields -> mapList.add(searchHitFields.getSourceAsMap()));
+        return mapList;
+    }
 
+    public TransportClient getClient() {
+        return client;
+    }
 
+    public void setClient(TransportClient client) {
+        this.client = client;
+    }
 }
