626d6a0346a50dcf993987d63566ce40f916df03
timewang <timewmoon@163.com>
2019-02-27 08:55:27 +0800

    降级与流控

--- dy-app-user-feature/src/main/java/com/globalegrow/dy/zaful/app/user/feature/controller/UserFeatureController.java
+++ dy-app-user-feature/src/main/java/com/globalegrow/dy/zaful/app/user/feature/controller/UserFeatureController.java
@@ -1,5 +1,6 @@
 package com.globalegrow.dy.zaful.app.user.feature.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.globalegrow.dy.controller.CommonController;
 import com.globalegrow.dy.zaful.app.user.feature.dto.AppUserFeatureRequest;
 import com.globalegrow.dy.zaful.app.user.feature.dto.AppUserFeatureResponse;
@@ -18,9 +19,16 @@ public class UserFeatureController extends CommonController {
     @Autowired
     private AppUserFeatureService zafulAppUserFeatureService;
 
+    @SentinelResource(value = "user_base_info_fbad_feature", blockHandler = "failed", fallback = "failed")
     @PostMapping(produces = "application/json;charset=UTF-8")
     public AppUserFeatureResponse getUserFeature(@Validated @RequestBody AppUserFeatureRequest request) {
         return this.zafulAppUserFeatureService.appUserFeatures(request);
+    }
+
+    public AppUserFeatureResponse failed() {
+        AppUserFeatureResponse response = new AppUserFeatureResponse();
+        response.setSuccess(false);
+        return response;
     }
 
 }
