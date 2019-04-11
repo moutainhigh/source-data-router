b556628c5f7b233c911c9729ded05c202995e48d
timewang <timewmoon@163.com>
2019-04-11 08:55:12 +0800

    组织导入

--- flink-job-scheduler/src/main/java/com/globalegrow/fixed/scheduler/BuryLogDataSyncByDay.java
+++ flink-job-scheduler/src/main/java/com/globalegrow/fixed/scheduler/BuryLogDataSyncByDay.java
@@ -10,12 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
