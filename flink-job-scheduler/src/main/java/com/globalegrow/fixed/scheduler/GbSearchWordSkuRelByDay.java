72d0076cb4ebfedaba74bad27b157e62f047c33b
timewang <timewmoon@163.com>
2019-04-23 15:05:57 +0800

    搜索词与 sku 对应关系

--- flink-job-scheduler/src/main/java/com/globalegrow/fixed/scheduler/GbSearchWordSkuRelByDay.java
+++ flink-job-scheduler/src/main/java/com/globalegrow/fixed/scheduler/GbSearchWordSkuRelByDay.java
@@ -20,7 +20,7 @@ public class GbSearchWordSkuRelByDay extends AbstractFlinkJobSerialScheduler{
 
     String hdfsPath = "/user/hive/warehouse/dw_proj.db/zaful_app_search_word_fourteen_days_report/add_time=";
 
-    String commandLine = "/usr/local/services/flink/flink-yarn/flink-1.5.0/bin/flink run -d /usr/local/services/flink/search-words-skus-rel-0.1.jar --elastic-servers 172.31.47.84:9302,172.31.43.158:9302,172.31.55.231:9302 --index-name dy_gb_search_words_skus_rel --type-name log --cluster.name esearch-aws-dy --job.path.auto.end false --job.search.words.path ";
+    String commandLine = "/usr/local/services/flink/flink-yarn/flink-1.5.0/bin/flink run -d -m yarn-cluster -yn 1 -yjm 1024 -ytm 1024 -yqu root.flink /usr/local/services/flink/search-words-skus-rel-0.1.jar --elastic-servers 172.31.47.84:9302,172.31.43.158:9302,172.31.55.231:9302 --index-name dy_gb_search_words_skus_rel --type-name log --cluster.name esearch-aws-dy --job.path.auto.end false --job.search.words.path ";
 
     @Override
     @Scheduled(cron = "0 15 5 * * ?")
