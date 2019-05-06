22954f3561bcc70654e51f52c66fc92edf32e22e
timewang <timewmoon@163.com>
2019-05-06 15:49:54 +0800

    新增游客回访类型

--- dy-app-user-feature/src/main/java/com/globalegrow/dy/zaful/app/user/feature/dto/AppUserLayerResponse.java
+++ dy-app-user-feature/src/main/java/com/globalegrow/dy/zaful/app/user/feature/dto/AppUserLayerResponse.java
@@ -8,9 +8,10 @@ import lombok.ToString;
 @Data
 @ToString
 @ApiModel("用户层级数据响应对象，用户不存在时返回空字符串，data 类型为 string，\n" +
-        "00：老用户，无回访 \n" +
-        "01：老用户，有回访 \n" +
-        "10：新用户，无回访 \n" +
-        "11：新用户，有回访")
+        "000：老用户，无回访 <br>" +
+        "010：老用户，有回访 <br>" +
+        "100：新用户，无回访 <br>" +
+        "110：新用户，有回访 <br>" +
+        "111：游客回访")
 public class AppUserLayerResponse extends CommonPatternResponse<String> {
 }
