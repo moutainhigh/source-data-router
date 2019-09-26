c3603c214255842316fad2cfe5369a313650a20a
xuqiufeng <xuqiufeng@globalegrow.com>
2019-09-26 11:59:02 +0800

    flik -job-schedule  启动命令忘记加filepath没有拼接hdfs namenode文件，任务跑不动，但是也报success

--- flink-job-scheduler/src/main/java/com/globalegrow/fixed/scheduler/GbAppUserEventByDay.java
+++ flink-job-scheduler/src/main/java/com/globalegrow/fixed/scheduler/GbAppUserEventByDay.java
@@ -18,7 +18,7 @@ import java.util.concurrent.DelayQueue;
  */
 @Slf4j
 @Component
-public class GbAppUserEventByDay extends AbstractFlinkJobSerialScheduler{
+public class GbAppUserEventByDay extends AbstractFlinkJobSerialScheduler {
 
 
     @Autowired
@@ -27,7 +27,7 @@ public class GbAppUserEventByDay extends AbstractFlinkJobSerialScheduler{
 
     String hdfsPath = "/bigdata/ods/log_clean/ods_app_burial_log/${last_day}/gearbest";
 
-    String commandLine = "/usr/local/services/flink/flink-yarn/flink-1.5.0/bin/flink run -m yarn-cluster -yn 1 -yjm 1024 -ytm 1024 -yqu root.flink -ynm gb-app-user-event-init-everyday /usr/local/services/flink/hdfs-orc-file-es-0.1.jar --elastic-servers 172.31.47.84:9302,172.31.43.158:9302,172.31.55.231:9302 --index-name dy_app_gb_event --type-name log --cluster.name esearch-aws-dy --es.eventIndex true --es.routing true --job-name history-data-2018-01 --hdfs.server ${name_node_server1} --filePath ${yestoday_file1} --query.sql \"select event_name,event_value,appsflyer_device_id,customer_user_id,event_time ,app_name,platform from user_event where event_name in('af_view_product','af_add_to_bag','af_add_to_wishlist','af_create_order_success','af_search','af_purchase') and lower(app_name) like '%gearbest%'\" --job.parallelism 1 ";
+    String commandLine = "/usr/local/services/flink/flink-yarn/flink-1.5.0/bin/flink run -d -m yarn-cluster -yn 1 -yjm 1024 -ytm 1024 -yqu root.flink -ynm gb-app-user-event-init-everyday /usr/local/services/flink/hdfs-orc-file-es-0.1.jar --elastic-servers 172.31.47.84:9302,172.31.43.158:9302,172.31.55.231:9302 --index-name dy_app_gb_event --type-name log --cluster.name esearch-aws-dy --es.eventIndex true --es.routing true --hdfs.server ${name_node_server1} --filePath ${yestoday_file1} --query.sql \"select event_name,event_value,appsflyer_device_id,customer_user_id,event_time ,app_name,platform from user_event where event_name in('af_view_product','af_add_to_bag','af_add_to_wishlist','af_create_order_success','af_search','af_purchase') and lower(app_name) like '%gearbest%'\" --job.parallelism 1 ";
 
 
     /**
@@ -35,15 +35,16 @@ public class GbAppUserEventByDay extends AbstractFlinkJobSerialScheduler{
      */
     @Override
     @Scheduled(cron = "${app.cron.gb-app-user-event:0 10 3 * * ?}")
-    public void run()throws InterruptedException{
+    public void run() throws InterruptedException {
         log.info("gb app用户event 任务运行");
         String yesterdayhdfsPath = CommonTextUtils.replaceOneParameter(this.hdfsPath, "last_day",
                 DateUtil.yesterday().toString("yyyy/MM/dd"));
         this.checkHdfsPath("hdfs://glbgnameservice" + yesterdayhdfsPath);
-        String bigdatahdfsPath =  HdfsUtil.getBigDataActiveNamenode();
-        String hdfsCommad=CommonTextUtils.replaceOneParameter(this.commandLine, "name_node_server1",
+        String bigdatahdfsPath = HdfsUtil.getBigDataActiveNamenode();
+        String hdfsCommad = CommonTextUtils.replaceOneParameter(this.commandLine, "name_node_server1",
                 bigdatahdfsPath);
-        String finalCommad=CommonTextUtils.replaceOneParameter(hdfsCommad, "yestoday_file1", yesterdayhdfsPath);
+        String finalCommad = CommonTextUtils.replaceOneParameter(hdfsCommad, "yestoday_file1",
+                bigdatahdfsPath.concat(yesterdayhdfsPath));
         FlinkBashJob job = new FlinkBashJob("gb-app-user-event-init-everyday", finalCommad);
         this.flinkBashJobs.offer(job);
         this.runFlinkJob();
