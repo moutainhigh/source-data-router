3c621f581b7e329270e38e6016bbea312401265a
xuqiufeng <xuqiufeng@globalegrow.com>
2019-10-09 18:20:37 +0800

    flik -job-schedule  test

---
+++ flink-job-scheduler/src/main/java/com/globalegrow/fixed/scheduler/TestRunnerAction.java
@@ -0,0 +1,17 @@
+package com.globalegrow.fixed.scheduler;
+
+/**
+ * @Auther: joker
+ * @Date: 2019/10/7 18:55
+ * @Description:
+ */
+public class TestRunnerAction {
+    public static void main(String[] args) throws Exception {
+        TestRunner testRunner = new TestRunner();
+        testRunner.run();
+    }
+}
+
+
+
+
