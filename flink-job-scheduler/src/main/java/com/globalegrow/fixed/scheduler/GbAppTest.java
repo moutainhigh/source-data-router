3e2f8cc8905cc39df5f4301d98689f4ce2c4fcbb
xuqiufeng <xuqiufeng@globalegrow.com>
2019-10-09 17:24:38 +0800

    flik -job-schedule

---
+++ flink-job-scheduler/src/main/java/com/globalegrow/fixed/scheduler/GbAppTest.java
@@ -0,0 +1,13 @@
+package com.globalegrow.fixed.scheduler;
+
+/**
+ * @Auther: joker
+ * @Date: 2019/10/9 17:23
+ * @Description:
+ */
+public class GbAppTest {
+    public static void main(String[] args) throws Exception {
+        GbAppUserEventByDay gbAppUserEventByDay = new GbAppUserEventByDay();
+        gbAppUserEventByDay.run();
+    }
+}
--- flink-job-scheduler/src/main/java/com/globalegrow/fixed/scheduler/GbAppUserEventByDay.java
+++ flink-job-scheduler/src/main/java/com/globalegrow/fixed/scheduler/GbAppUserEventByDay.java
@@ -20,8 +20,7 @@ public class GbAppUserEventByDay extends AbstractFlinkJobSerialScheduler {
 
     String hdfsPath = "/bigdata/ods/log_clean/ods_app_burial_log/${last_day}/gearbest";
 
-    String commandLine = "/usr/local/services/flink/flink-yarn/flink-1.5.0/bin/flink run -d -m " +
-            "yarn-cluster -yn 1 -yjm 1024 -ytm 1024 -yqu root.flink -ynm gb-app-user-event-lastday /usr/local/services/flink/hdfs-orc-file-es-0.1.jar --elastic-servers 172.31.47.84:9302,172.31.43.158:9302,172.31.55.231:9302 --index-name dy_app_gb_event --type-name log --cluster.name esearch-aws-dy --es.eventIndex true --es.routing true --hdfs.server $name_node_server1 --filePath $yestoday_file1 --query.sql \"select event_name,event_value,appsflyer_device_id,customer_user_id,event_time ,app_name,platform from user_event where event_name in('af_view_product','af_add_to_bag','af_add_to_wishlist','af_create_order_success','af_search','af_purchase') and lower(app_name) like '%gearbest%'\" --job.parallelism 1";
+    String commandLine = "/usr/local/services/flink/flink-yarn/flink-1.5.0/bin/flink run -d -m yarn-cluster -yn 1 -yjm 1024 -ytm 1024 -yqu root.flink -ynm gb-app-user-event-init-everyday /usr/local/services/flink/hdfs-orc-file-es-0.1.jar --elastic-servers 172.31.47.84:9302,172.31.43.158:9302,172.31.55.231:9302 --index-name dy_app_gb_event --type-name log --cluster.name esearch-aws-dy --es.eventIndex true --es.routing true --hdfs.server hdfs://172.31.20.96:8020 --filePath hdfs://172.31.20.96:8020/bigdata/ods/log_clean/ods_app_burial_log/2019/10/08/gearbest --query.sql \"select event_name,event_value,appsflyer_device_id,customer_user_id,event_time,app_name,platform from user_event where event_name in('af_view_product','af_add_to_bag','af_add_to_wishlist','af_create_order_success','af_search','af_purchase') and lower(app_name) like '%gearbest%'\" --job.parallelism 1";
 
 
     /**
